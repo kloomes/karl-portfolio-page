df190dc8120a131de7d0ab8bb2b0a458e071ea99
Karl Loomes <kloomes@hotmail.com>
2020-06-05 14:46:38 +0100

    Portfolio index skeleton in place

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,4 +1,7 @@
 class Website < ApplicationRecord
   has_one_attached :full_website
   has_one_attached :mobile_website
+
+  validates :full_website, presence: true
+  validates :mobile_website, presence: true
 end
